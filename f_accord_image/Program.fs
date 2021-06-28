6e658715b3eafc8fd3f3a3e7d8657f8442801ec0
Эмилия <melnikova.emiliya.prog@gmail.com>
2021-06-29 00:43:56 +0300

    image project with accord.net

---
+++ f_accord_image/Program.fs
@@ -0,0 +1,51 @@
+﻿open System
+open System.IO
+
+open Accord.MachineLearning
+open Accord.MachineLearning.VectorMachines
+open Accord.MachineLearning.VectorMachines.Learning
+open Accord.Statistics.Kernels
+let training = @"C:/Users/binar/RiderProjects/f_accord_image/f_accord_image/bin/Debug/netcoreapp3.1/data/trainingsample.csv"
+let validation = @"C:/Users/binar/RiderProjects/f_accord_image/f_accord_image/bin/Debug/netcoreapp3.1/data/validationsample.csv"
+
+let readData filePath =
+    File.ReadAllLines filePath
+    |> fun lines -> lines.[1..]
+    |> Array.map (fun line -> line.Split(','))
+    |> Array.map (fun line -> 
+        (line.[0] |> Convert.ToInt32), (line.[1..] |> Array.map Convert.ToDouble))
+    |> Array.unzip
+
+let labels, observations = readData training
+
+let features = 28 * 28
+let classes = 10
+
+let algorithm = 
+    fun (svm: KernelSupportVectorMachine) 
+        (classInputs: float[][]) 
+        (classOutputs: int[]) (i: int) (j: int) -> 
+        let strategy = SequentialMinimalOptimization(svm, classInputs, classOutputs)
+        strategy :> ISupportVectorMachineLearning
+
+let kernel = Linear()
+let svm = new MulticlassSupportVectorMachine(features, kernel, classes)
+let learner = MulticlassSupportVectorLearning(svm, observations, labels)
+let config = SupportVectorMachineLearningConfigurationFunction(algorithm)
+learner.Algorithm <- config
+
+let error = learner.Run()
+
+printfn "Error: %f" error
+
+let validationLabels, validationObservations = readData validation
+
+let correct =
+    Array.zip validationLabels validationObservations 
+    |> Array.map (fun (l, o) -> if l = svm.Compute(o) then 1. else 0.)
+    |> Array.average
+
+let view =
+    Array.zip validationLabels validationObservations 
+    |> fun x -> x.[..20]
+    |> Array.iter (fun (l, o) -> printfn "Real: %i, predicted: %i" l (svm.Compute(o)))
